2f524937ddea58b8e8a8b42cb1c8c24893d8de6d
QuratFaheem <mrs.faheem16@gmail.com>
2024-11-09 18:35:22 +0500

    done

--- src/app/components/transition.tsx
+++ src/app/components/transition.tsx
@@ -1,5 +1,6 @@
-
 "use client";
+
+import { motion } from "framer-motion";
 
 export default function Transition({
   children,
@@ -7,12 +8,12 @@ export default function Transition({
   children: React.ReactNode;
 }) {
   return (
-    <div
+    <motion.div
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ ease: "easeInOut", duration: 0.75 }}
     >
       {children}
-    </div>
+    </motion.div>
   );
 }
